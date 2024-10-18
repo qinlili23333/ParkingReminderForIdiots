607ef67cd807cf25083ba7e0099106163487c7c6
琴梨梨OvO <24567775+qinlili23333@users.noreply.github.com>
2024-10-18 21:44:51 +1000

    支持保存配置

--- app/src/main/java/moe/qinlili/parkingreminderforidiots/MainActivity.java
+++ app/src/main/java/moe/qinlili/parkingreminderforidiots/MainActivity.java
@@ -11,6 +11,10 @@ import android.os.PowerManager;
 import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.Switch;
 import android.widget.Toast;
 
 
@@ -20,6 +24,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        readConfig();
         refreshBatteryWhitelist();
     }
     @Override
@@ -31,7 +36,23 @@ public class MainActivity extends Activity {
 
     protected void readConfig(){
         SharedPreferences pref= getSharedPreferences("config",0);
+        ((Switch)findViewById(R.id.enable_msg)).setChecked(pref.getBoolean("enable_msg",false));
+        ((Switch)findViewById(R.id.enable_jump)).setChecked(pref.getBoolean("enable_jump",false));
+        ((EditText)findViewById(R.id.ssid)).setText(pref.getString("ssid","UQ"));
+        ((EditText)findViewById(R.id.pkgname)).setText(pref.getString("pkgname","air.com.cellopark.au"));
+        ((RadioGroup)findViewById(R.id.cool_cd)).check(pref.getInt("cool_cd",R.id.cd_day));
+    }
 
+    public void saveConfig(View view) {
+        SharedPreferences pref = getSharedPreferences("config", 0);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putBoolean("enable_msg", ((Switch)findViewById(R.id.enable_msg)).isChecked());
+        editor.putBoolean("enable_jump", ((Switch)findViewById(R.id.enable_jump)).isChecked());
+        editor.putString("ssid", ((EditText) findViewById(R.id.ssid)).getText().toString());
+        editor.putString("pkgname", ((EditText) findViewById(R.id.pkgname)).getText().toString());
+        editor.putInt("cool_cd", ((RadioGroup) findViewById(R.id.cool_cd)).getCheckedRadioButtonId());
+        editor.apply();
+        Toast.makeText(this, "保存成功", Toast.LENGTH_SHORT).show();
     }
 
     @SuppressLint("BatteryLife")
